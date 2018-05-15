3653bedc1775d5ae637efbaf7489d99b03ccef95
u211-17 <u211-17@public.mpei.local>
2018-05-15 10:28:02 +0300

    3

--- main.cpp
+++ main.cpp
@@ -29,10 +29,41 @@ void print_in_hex(const void*data,size_t size)
         else {
             cout << ' ';
         }
-
     }
-
 }
+
+char
+bit_digit(uint8_t byte, uint8_t bit) {
+    if (byte & (0x1 << bit)) {
+        return '1';
+    }
+    return '0';
+}
+
+void
+print_in_binary(uint8_t byte) {
+    for (uint8_t bit = 7; bit > 0; bit--) {
+        cout << bit_digit(byte, bit);
+    }
+}
+
+
+void
+print_in_binary(const void* data, size_t size) {
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++) {
+        print_in_binary(bytes[i]);
+
+        if ((i + 1) % 4 == 0) {
+            cout << '\n';
+        }
+        else {
+            cout << ' ';
+        }
+    }
+}
+
+
 
 int
 main() {
@@ -56,5 +87,6 @@ main() {
     cout << "u32 bytes: ";
     print_in_hex(&u32, sizeof(u32));
     cout << '\n';
+
     return 0;
 }
